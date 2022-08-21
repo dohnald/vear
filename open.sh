f07818f1db82f7cc694df29c010dd52cf8930061
d.ohnald <77563784+dohnald@users.noreply.github.com>
2022-08-22 08:50:13 +0900

    Create open.sh

---
+++ open.sh
@@ -0,0 +1,11 @@
+DIR="$HOME/vear"
+TOKEN=`cat $DIR/.token`
+if [ $# -eq 0 ]
+  then
+    RESULT=$($DIR/lib/xcall.app/Contents/MacOS/xcall -url "bear://x-callback-url/search?term=@last3days&token=$TOKEN&show_window=no" -activateApp NO | jq -r ".notes" | jq -r ".[] | .title+\"|\"+.identifier" | fzf | awk -F '|' '{print $NF}')
+else
+    RESULT=$1
+fi
+echo $RESULT
+$DIR/lib/xcall.app/Contents/MacOS/xcall -url "bear://x-callback-url/open-note?open_note=no&show_window=no&id=$RESULT" | jq -r '.note' > $DIR/$RESULT.md
+nvim $DIR/$RESULT.md
